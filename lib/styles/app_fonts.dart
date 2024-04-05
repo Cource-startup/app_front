e2aec7635c553ec1d9b7c1e7eadaadd71b69f5a8
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-04-05 02:14:14 -0400

    feature: ready splash_screen

--- lib/styles/app_fonts.dart
+++ lib/styles/app_fonts.dart
@@ -3,11 +3,10 @@ import 'app_colors.dart';
 
 class AppFonts {
   static TextStyle h1TextStyle = TextStyle(
-    fontFamily: 'Baloo2',
-    fontSize: 26, 
-    color: AppColors.back, 
-    fontWeight: FontWeight.bold
-  );
+      fontFamily: 'Baloo2',
+      fontSize: 72,
+      color: AppColors.white,
+      fontWeight: FontWeight.w900);
 
   static TextStyle h2TextStyle = TextStyle(
       fontSize: 21, color: AppColors.back, fontWeight: FontWeight.bold);
--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app_front/styles/app_fonts.dart';
+import 'package:app_front/styles/styles.dart';
 import 'package:app_front/views/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -15,17 +16,22 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen())));
+    Timer(Duration(seconds: 2), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen())));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.firstBrand,
       body: Center(
-        child: Column (children: [
-          SvgPicture.asset('assets/images/logo__full_color_white_eyes.svg'),
-          const SizedBox(height: 20,),
-          Text("FURELY", style: AppFonts.h1TextStyle,),
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Padding(padding: EdgeInsets.only(bottom: 20), 
+            child: SvgPicture.asset('assets/images/logo__full_color_white_eyes.svg'),
+          ),
+          Text("FURELY", style: AppFonts.h1TextStyle,)
         ]),
       ),
     );
