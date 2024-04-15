0e7d172949969614bbcd437a6492a07a7d97be67
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-04-15 01:05:35 -0400

    refactor: rebuild buttons code

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -1,25 +1,10 @@
 import 'package:app_front/styles/styles.dart';
-import 'package:app_front/views/widgets/auth_button.dart';
+import 'package:app_front/views/widgets/auth_buttons/google_auth_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatelessWidget {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  LoginScreen({super.key});
-
-  Future<GoogleSignInAccount?> _handleSignIn() async {
-    try {
-      // Google Sign In configuration object.
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      print(googleUser);
-      return googleUser;
-    } catch (error) {
-      print(error);
-      return null;
-    }
-  }
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +20,7 @@ class LoginScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 60),
+                    padding: const EdgeInsets.symmetric(horizontal: 60),
                     child: SvgPicture.asset(
                         'assets/images/logo__full_color_white_eyes.svg'),
                   ),
@@ -49,14 +34,7 @@ class LoginScreen extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 36, left: 16, right: 16),
-              child: AuthButton(
-                  iconPath: 'assets/images/auth/google.svg',
-                  text: 'Continue with Google',
-                  onPressed: () {
-                    _handleSignIn().then((GoogleSignInAccount? user) {
-                      print(user!.displayName);
-                    }).catchError((e) => print(e));
-                  }),
+              child: GoogleAuthButton(),
             ),
           ],
         ),
