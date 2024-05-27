8577d36f269fe67bb9ee49a9da7eb9c19a978f68
TimaliveProgman <timur20051610@gmail.com>
2024-05-27 09:05:43 +0500

    for backend and frontend linking

--- lib/views/widgets/auth_buttons/google_auth_button.dart
+++ lib/views/widgets/auth_buttons/google_auth_button.dart
@@ -1,13 +1,17 @@
+import 'dart:convert';
+
 import 'package:app_front/views/screens/registration_screen.dart';
 import 'package:app_front/views/widgets/screen_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
 
 class GoogleAuthButton extends StatelessWidget {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
-    clientId: Platform.isAndroid ? dotenv.env['GOOGLE_AUTH_API_CLIENT_ID'] : null,
+    serverClientId:
+        Platform.isAndroid ? dotenv.env['GOOGLE_AUTH_API_CLIENT_ID'] : null,
   );
 
   GoogleAuthButton({
@@ -16,13 +20,31 @@ class GoogleAuthButton extends StatelessWidget {
 
   Future<GoogleSignInAccount?> _handleSignIn(BuildContext context) async {
     try {
-      // Google Sign In configuration object.
+      String url = 'http://localhost:8888/checking';
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      print(googleUser);
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => RegistrationScreen()),
-      );
+      if (googleUser != null) {
+        Map<String, dynamic> userData = {
+          'displayName': googleUser.displayName,
+          'email': googleUser.email,
+          'id': googleUser.id,
+          'photoUrl': googleUser.photoUrl,
+          'serverAuthCode': googleUser.serverAuthCode,
+        };
+
+        final response = await http.post(
+          Uri.parse(url),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode(userData),
+        );
+        if (response.statusCode == 200) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => RegistrationScreen()),
+          );
+        } else {
+          print('Failed to send user data: ${response.statusCode}');
+        }
+      }
       return googleUser;
     } catch (error) {
       print("Error:  ");
